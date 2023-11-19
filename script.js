7ee75357b1a219b8e12548297c8d1e0bb65b818e
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-19 16:41:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstWord(s) {
   // your code here
+	let ans="";
+	for(let i=0;i<s.length;i++){
+		if(s.charAt(i)!==" "){
+			for(let j=i;j<s.length;j++){
+				if(s.charAt(j)==" "){
+					return ans;
+				}
+				else{
+					ans+=s.charAt(j);
+				}
+			}
+			return ans;
+		}
+	}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
